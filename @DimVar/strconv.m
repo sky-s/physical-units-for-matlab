c8dfc8e3ac8d94a898a791707821509fb5bd7d15
Sky Sartorius <Sky.Sartorius@gmail.com>
2017-03-17 17:03:53 -0700

    added unitConversionFactor

--- @DimVar/strconv.m
+++ @DimVar/strconv.m
@@ -1,27 +1,26 @@
-function [cTo,cInverse] = strconv(sFrom,sTo)
-% [cTo,cInverse] = STRCONV(sFrom,sTo) finds conversion factor, cTo, that
-% converts from the unit indicated by the string sFrom to the unit indicated by
-% the string sTo. cInverse is the inverse.
+function [cTo,cInverse] = strconv(sFrom,sTo,varargin)
+% strconv  Find conversion factor between units defined by strings.
+% 
+%   [c, cInv] = STRCONV(sFrom, sTo) finds the conversion factor that converts
+%   from the unit indicated by the string sFrom to the unit indicated by the
+%   string sTo such that sFrom = c*sTo and sTo = cInv*sFrom. If the units are
+%   not compatible, an error is thrown.
+%
+%   [c, cInv] = unitConversionFactor(sFrom, sTo, 'Force', true) allows for
+%   incompatible units, in which case the function may return DimVars instead of
+%   throwing an error.
 % 
 %   Examples:
 %       DimVar.STRCONV('MPa/min','(lbf/cm^2)/hr')
 %       DimVar.STRCONV('deg/min','1/hr')
 % 
-%   See also u, str2u.
+%   See also u, str2u, unitConversionFactor.
 
-sTo = str2u(sTo);
-sFrom = str2u(sFrom);
-
-cTo = sFrom/sTo;
-
-if isa(cTo, 'DimVar')
-    error('Incompatible units.')
+if ischar(sFrom)
+    sFrom = str2u(sFrom);
+end
+if ischar(sTo)
+    sTo = str2u(sTo);
 end
 
-cInverse = 1/cTo;
-
-end
-
-% 2017-01-21 replaced subfunction with new str2u, enabling simplification
-% (removing subfunction, removing scrubbing inputs for empty strings, removing
-% eval call, ...).
+[cTo,cInverse] = unitConversionFactor(sFrom,sTo,varargin{:});
---
+++ @DimVar/unitConversionFactor.m
@@ -0,0 +1,37 @@
+function [cTo,cInverse] = unitConversionFactor(from,to,varargin)
+% unitConversionFactor  Find conversion factor between units.
+% 
+%   [c, cInv] = unitConversionFactor(FROM, TO) returns a factor c such that FROM
+%   is equal to c*TO and TO = cInv*FROM. If the units of FROM and TO are not
+%   compatible, an error is thrown.
+%
+%   [c, cInv] = unitConversionFactor(FROM, TO, 'Force', true) allows for
+%   incompatible units, in which case the function may return DimVars instead of
+%   throwing an error. This is the same as calling c = FROM./TO.
+% 
+%   See also u, strconv, compatible, symunit/unitConversionFactor.
+
+if ischar(from)
+    from = str2u(from);
+end
+if ischar(to)
+    from = str2u(to);
+end
+
+p = inputParser;
+p.addParameter('Force',false);
+p.parse(varargin{:});
+
+if p.Results.Force
+    % Do nothing if anything was provided for Force (string, double, etc.).
+else
+    % Throw error if incompatible.
+    compatible(from,to);
+end
+
+
+cTo = from./to;
+
+cInverse = 1./cTo;
+
+end
