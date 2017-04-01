11aab3027ccb809c94a9faa65e7d94d9ac7d3ca3
Sky Sartorius <Sky.Sartorius@gmail.com>
2017-04-01 10:33:27 -0700

    double and displayingvalue methods added

---
+++ @DimVar/double.m
@@ -0,0 +1,6 @@
+function vOut = double(v)
+% DimVar.double(V)  Returns double(u2num(V)).
+% 
+% See also u2num.
+
+vOut = double(u2num(v));
---
+++ displayingvalue.m
@@ -0,0 +1,10 @@
+function v = displayingvalue(v)
+% DISPLAYINGVALUE(V)  Returns value of DimVar used in displaying and plotting
+% (which varies with selected base unit systems and preferred display units).
+% 
+%   See also makehgtform, DimVar.display, DimVar.u2num, DimVar.plot.
+
+if isa(v,'DimVar')
+    [~, ~, v] = display(v);
+    v = double(v);
+end
