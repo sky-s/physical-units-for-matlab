babc1625afaf2641d0fae83e757d56d8dca3ad91
Sky Sartorius <Sky.Sartorius@gmail.com>
2017-10-19 15:07:33 -0700

    num2str: modified to allow usage with tables (behavior change and some workarounds added

--- @DimVar/num2str.m
+++ @DimVar/num2str.m
@@ -1,29 +1,43 @@
 function [s, appendString, v, val] = num2str(v, varargin)
-
 [numeratorString, denominatorString, v] = display(v);
 val = v.value;
-s = num2str(val, varargin{:});
-[m, ~] = size(s);
 
+% Compensation for behavior of @tabular\disp.m trying to format with %d:
+ST = dbstack(1,'-completenames');
+if isequal(varargin,{'%d    '}) && ...
+        contains(ST(1).file,['@tabular' filesep 'disp.m'])
+    varargin = {getFloatFormats()}; % Assume double value.
+end
+
+s = num2str(val, varargin{:});
 
 if ~isempty(denominatorString)
     denominatorString = ['/' denominatorString];
 end
 appendString = sprintf('%s%s', numeratorString, denominatorString);
 
-if m > 1
-    % Multiline.
-    s = char(s,appendString);
-else
-    % Single line.
-    if isempty(s)
-        s = '[]';
-    end
-    
-    [~,n] = size(val);
-    if n > 1
-        % Indicate row vector using brackets.
-        s = ['[' s ']'];
-    end
-    s = [s ' ' appendString];
+if isempty(s)
+    s = '[]';
+end
+
+s = strcat(s,[' ' appendString]);
+end
+
+
+function dblFmt = getFloatFormats()
+
+switch lower(matlab.internal.display.format)
+    case {'short' 'shortg' 'shorteng'}
+        dblFmt  = '%.5g    ';
+    case {'long' 'longg' 'longeng'}
+        dblFmt  = '%.15g    ';
+    case 'shorte'
+        dblFmt  = '%.4e    ';
+    case 'longe'
+        dblFmt  = '%.14e    ';
+    case 'bank'
+        dblFmt  = '%.2f    ';
+    otherwise % rat, hex, + fall back to shortg
+        dblFmt  = '%.5g    ';
+end
 end
