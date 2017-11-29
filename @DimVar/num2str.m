08dc19a0576654d06b652c71713d4bf1c81f9b0b
Sky Sartorius <Sky.Sartorius@gmail.com>
2017-11-28 17:22:05 -0800

    re-added workaround for @tabular/disp format

--- @DimVar/num2str.m
+++ @DimVar/num2str.m
@@ -1,5 +1,13 @@
 function [s] = num2str(v, varargin)
 
+% Compensation for behavior of @tabular\disp.m trying to format with %d:
+ST = dbstack(1,'-completenames');
+if isequal(varargin,{'%d    '}) && ...
+        contains(ST(1).file,['@tabular' filesep 'disp.m'])
+    varargin = {getFloatFormats()}; % Assume double value.
+end
+
+
 [dispVal,~,unitStr] = displayparser(v);
 s = num2str(dispVal, varargin{:});
 
@@ -8,3 +16,23 @@ if isempty(s)
 end
 
 s = strcat(s,[' ' unitStr]);
+
+end
+
+function dblFmt = getFloatFormats()
+
+switch lower(matlab.internal.display.format)
+    case {'short' 'shortg' 'shorteng'}
+        dblFmt  = '%.5g    ';
+    case {'long' 'longg' 'longeng'}
+        dblFmt  = '%.15g    ';
+    case 'shorte'
+        dblFmt  = '%.4e    ';
+    case 'longe'
+        dblFmt  = '%.14e    ';
+    case 'bank'
+        dblFmt  = '%.2f    ';
+    otherwise % rat, hex, + fall back to shortg
+        dblFmt  = '%.5g    ';
+end
+end
