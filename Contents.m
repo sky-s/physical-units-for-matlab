56b22a4c17859a02fa646d22d704fcbf3847714e
Sky <sky-s@users.noreply.github.com>
2019-08-07 14:57:25 -0700

    removed unmaintained Contents.m

--- Contents.m
+++
@@ -1,7 +0,0 @@
-% UNITS Physical Units Toolbox
-% Version 3.1 14-Jul-2017 .
-%
-%   u                    - u  Physical units.
-%   displayUnits         - displayUnits  Sets preferred units for display and plotting.
-%   baseUnitSystem       - baseUnitSystem  Sets base units (advanced).
-%   unitconversionfactor - unitconversionfactor  Find conversion factor between units.
