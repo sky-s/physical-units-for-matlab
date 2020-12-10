8b88adbe3872e3f9dd948115d4cbadb523b3c518
Sky <sky-s@users.noreply.github.com>
2020-12-09 22:21:30 -0800

    added Contents.m

---
+++ Contents.m
@@ -0,0 +1,16 @@
+% Physical Units Toolbox
+% Version 4.2 .
+%
+%   u                    - Units constants.
+%   scd                  - Set Custom Display units on a per-variable basis. 
+%   displayUnits         - List of preferred display units.
+%   baseUnitSystem       - Sets the base set of fundamental units.
+%   DimVar               - Dimensioned variable data type.
+%   str2u                - Convert a string to DimVar.
+%   unitconversionfactor - Find conversion factor between units.
+% 
+% https://www.mathworks.com/matlabcentral/fileexchange/38977
+% https://github.com/sky-s/physical-units-for-matlab
+% 
+% Copyright (c), Sky Sartorius. All rights reserved.
+% Contact: www.mathworks.com/matlabcentral/fileexchange/authors/101715 
