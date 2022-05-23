3bb16f89dc39a85a5f0e0396d7056e85042b4035
Dimitrii Nikolaev <nikolaev@ift.at>
2022-05-23 10:23:12 +0200

    fixed issue with wrong behaviour on baseUnitSystem if called from a subroutine

--- .tests/testScript_noBaseUnits.m
+++ .tests/testScript_noBaseUnits.m
@@ -7,12 +7,12 @@ clear all
 baseUnitSystem = 'none';
 
 %% Normal
-assert(1==u.m)  %FIXME: fails with `Incompatible units. Cannot perform operation on variables with different units.` what is an expected behaviour of comparing a number with unit? magnitude comparison?
-assert(0.3048==u.ft)%FIXME: fails with `Incompatible units. Cannot perform operation on variables with different units.` what is an expected behaviour of comparing a number with unit? magnitude comparison?
+assert(1==u.m)
+assert(0.3048==u.ft)
 
 %% Offset
 assert(isa(u.degC,'OffsetDimVar'))
-assert(278.15==5*u.degC)%FIXME: fails with `Incompatible units. Cannot perform operation on variables with different units.` what is an expected behaviour of comparing a number with unit? magnitude comparison?
+assert(278.15==5*u.degC)
 assert((5*u.degC)/u.degC==5)
 
 %% Cleanup
