e7247614fcb0b569602b08dbb4b0a36365fa7e73
sky-s <sky-s@users.noreply.github.com>
2023-06-01 21:29:56 -0700

    fixed cumtrapz for single input

--- @DimVar/cumtrapz.m
+++ @DimVar/cumtrapz.m
@@ -1,18 +1,21 @@
-function vOut = cumtrapz(v1,v2,varargin)
+function vOut = cumtrapz(v1,varargin)
 
-
-if ~isa(v2,'DimVar') % v1 is the DimVar.
+if nargin == 1 || (nargin > 1 && ~isa(varargin{1},'DimVar'))
+    % v1 is the only DimVar.
     vOut = v1;
-    vOut.value = cumtrapz(v1.value,v2,varargin{:});
+    vOut.value = cumtrapz(v1.value,varargin{:});
 
-
-elseif ~isa(v1,'DimVar') % v2 is the DimVar.
+elseif ~isa(v1,'DimVar')
+    % v2 is the only DimVar.
+    v2 = varargin{1};
     vOut = v2;
-    vOut.value = cumtrapz(v1,v2.value,varargin{:});
+    vOut.value = cumtrapz(v1,v2.value,varargin{2:end});
 
-else % BOTH v1 and v2 are DimVars.
+else 
+    % BOTH v1 and v2 are DimVars.
+    v2 = varargin{1};
     vOut = v1;
-    vOut.value = cumtrapz(v1.value,v2.value,varargin{:});
+    vOut.value = cumtrapz(v1.value,v2.value,varargin{2:end});
     vOut.exponents = v1.exponents + v2.exponents;
     
     vOut = clearcanceledunits(vOut);
