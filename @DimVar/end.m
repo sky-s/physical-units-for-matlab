c656fb376bdd69455e953b5ece43b0278a09b9d7
sky-s <sky-s@users.noreply.github.com>
2023-06-01 21:30:24 -0700

    simplified end function

--- @DimVar/end.m
+++ @DimVar/end.m
@@ -1,8 +1,2 @@
-function endIndex = end(vIn,indexToFindEnd,numberOfIndexes)
-sizeVector = size(vIn.value);
-
-if(numberOfIndexes == 1)
-    endIndex = max(sizeVector);
-else
-    endIndex = sizeVector(indexToFindEnd);
-end
+function ind = end(vIn,varargin)
+ind = builtin('end',vIn.value,varargin{:});
