e7a4f40fb0d90a40737f14d24dfc344307bf639f
sky-s <sky-s@users.noreply.github.com>
2025-08-29 10:00:06 -0700

    added mustBeNonDim validator

---
+++ mustBeNonDim.m
@@ -0,0 +1,13 @@
+function mustBeNonDim(x)
+% mustBeNonDim  Validate input is numeric and non-dimensional.
+%
+%   mustBeNonDim(X) throws an error if X is not a numeric array or if X is a
+%   dimensioned variable (a `DimVar` produced by the Physical Units Toolbox).
+%   This function is intended for use in input validation and assert-style
+%   checks inside functions that require plain numeric data.
+% 
+%   See also isnumeric, u.
+
+if ~isnumeric(x) || isa(x,'DimVar')
+    error('Value must be a non-dimensional numeric type.');
+end
